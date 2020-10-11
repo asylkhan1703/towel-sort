548f5ad8616712d066b4fd412fcf838b9011ee9f
Asylkhan Fazylov <asylkhan1703@gmail.com>
2020-10-11 11:46:21 +0500

    init: add solution of  the task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (arg) {
+  if (arg === undefined || arg === []) return [];
+
+  let newArr = [];
+  let counter = 2;
+  arg.flatMap((row) => {
+    if (counter % 2 === 0) {
+      newArr.push(...row);
+    } else {
+      newArr.push(...row.reverse());
+    }
+    counter += 1;
+  });
+  
+  return newArr;
+};
